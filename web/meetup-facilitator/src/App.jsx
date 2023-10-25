e7475cfdeceab988f5c1dbb038ad443f91d7aef4
Chris Chan <christakleungchan@gmail.com>
2023-10-24 22:37:19 -0400

    Implement Google auth and getting user account details

--- web/meetup-facilitator/src/App.jsx
+++ web/meetup-facilitator/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Button from './components/Button'
 import { useRef, useState } from 'react'
+import GoogleAuth from './features/GoogleAuth'
 
 function App() {
 
@@ -47,6 +48,7 @@ function App() {
 
   return (
     <>
+      <GoogleAuth />
       <div className='flex-col flex my-3 gap-1'>
         <label className='text-2xl' htmlFor='new-group-name'>Group Name:</label>
         <input ref={groupName} onChange={onChange} className={`border-2 p-2 ${(isValid ? '' : 'border-red-500')} focus:outline-none`} name='new-group-name' id='new-group-name' type='text' placeholder='Enter the group name here.'/>
