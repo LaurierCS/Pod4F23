d529502b57587dd11f9feb2d3e895fe1d61900df
Pablo Araujo Camacho <46763241+pabloj2001@users.noreply.github.com>
2023-10-28 11:38:32 -0400

    Revert "Chris/setup google auth"

--- web/meetup-facilitator/src/main.jsx
+++ web/meetup-facilitator/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  </GoogleOAuthProvider>
-  )
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
