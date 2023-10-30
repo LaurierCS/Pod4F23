4dc5b0161f0879dd24c19126d4fa303462851a85
Pedro <phfaraco@gmail.com>
2023-10-30 14:27:45 -0400

    move file loading to outside of the function

--- api/optimeet/activities/views.py
+++ api/optimeet/activities/views.py
@@ -7,14 +7,14 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import os
 
+here = os.path.dirname(os.path.abspath(__file__))
+    
+with open(os.path.join(here, 'Activities.json')) as file:
+    
+    activities = json.load(file)
+    
 @csrf_exempt
 @api_view(['GET'])
 def get_activities(request):
-    
-    here = os.path.dirname(os.path.abspath(__file__))
-    
-    with open(os.path.join(here, 'Activities.json')) as file:
-        
-        activities = json.load(file)
 
     return Response(activities)
