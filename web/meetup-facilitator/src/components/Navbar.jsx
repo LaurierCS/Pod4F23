8ed23b19fd3c3ab6254faea9ee7b53174aeb0a56
Chris Chan <christakleungchan@gmail.com>
2023-10-31 23:38:13 -0400

    Add navbar to homepage

--- web/meetup-facilitator/src/components/Navbar.jsx
+++ web/meetup-facilitator/src/components/Navbar.jsx
@@ -19,11 +19,11 @@ function Navbar() {
     
 
     return (
-        <div className='flex items-center justify-between'>
-            <div className="logo">Optimeet</div>
-            <div className="user-info">
+        <div className=' w-screen bg-gray-400 flex items-center justify-between p-4 absolute top-0 left-0'>
+            <div className="flex items-center">Optimeet</div>
+            <div className="user-info flex justify-evenly items-center gap-4">
                 <div className="name">{name}</div>
-                <img src={pictureURL} className="profilePicture" style={{height:'50 px', width:'50 px'}}></img>
+                <img src={pictureURL} className="profilePicture w-10 h-10 rounded-full" ></img>
             </div>
         </div>
     )
