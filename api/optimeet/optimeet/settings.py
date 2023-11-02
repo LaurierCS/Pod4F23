365924300110f56150f28374b656e098635df7bd
Pablo Araujo Camacho <pabloj2001@gmail.com>
2023-11-02 11:43:06 -0400

    Updated settings.py to connect to db using individual params

--- api/optimeet/optimeet/settings.py
+++ api/optimeet/optimeet/settings.py
@@ -82,9 +82,11 @@ WSGI_APPLICATION = 'optimeet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': 'my.cnf',
-        },
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASS"),
     }
 }
 
