4957f6b2fe8ddf176b2d094232969ac910975114
Marissa Angeles <marissa4mae@gmail.com>
2023-11-13 00:47:30 -0500

    addtime

--- web/meetup-facilitator/src/App.jsx
+++ web/meetup-facilitator/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import Home from './pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
-
+import TimeMain from './Timepage/TimeMain'
+import TimeHour from './Timepage/TimeHour'
 function App() {
 
 
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/login' element={<Login/>} />
+          <Route path='/time' element={<TimeMain/>}/>
+          <Route path='/timehour' element={<TimeHour/>}/>
         </Routes>
       </Router>
   )
--- web/meetup-facilitator/src/Timepage/TimeHour.jsx
+++ web/meetup-facilitator/src/Timepage/TimeHour.jsx
@@ -2,34 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function TimeHour() {
+  console.log("TimeHour rendered");
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const dates = JSON.parse(searchParams.get("dates"));
 
-  const [hourlyGrid, setHourlyGrid] = useState([]);
   const [selectedButtons, setSelectedButtons] = useState(new Set());
   const [formattedDates, setFormattedDates] = useState([]);
+  const buttonWidth = `calc(${100 / dates.length}%)`;
 
-  useEffect(() => {
-    const hours = Array.from({ length: 16 }, (_, index) => 8 + index);
-    const grid = hours.map((hour) => {
-      return dates.map((date) => {
-        const isSelected = selectedButtons.has(`${date}-${hour}`);
-        const buttonStyle = isSelected ? { background: "green" } : {};
-        return (
-          <button
-            key={`${date}-${hour}`}
-            style={buttonStyle}
-            onClick={() => handleButtonClick(date, hour)}
-          >
-            {hour}:00
-          </button>
-        );
-      });
-    });
-
-    setHourlyGrid(grid);
-  }, [dates, selectedButtons]);
+  const hours = Array.from({ length: 16 }, (_, index) => 8 + index);
 
   const handleButtonClick = (date, hour) => {
     const buttonKey = `${date}-${hour}`;
@@ -65,6 +47,38 @@ export default function TimeHour() {
     setFormattedDates(formattedSelection);
   };
 
+  // Creating the grid with buttons for each hour
+  const hourlyGrid = hours.map((hour) => {
+    // Creating buttons for each date
+    const buttons = dates.map((date) => {
+      const isSelected = selectedButtons.has(`${date}-${hour}`);
+      const buttonStyles = {
+        background: isSelected ? "green" : "white",
+        color: isSelected ? "white" : "black",
+        height: "36px", // Adjust as needed
+        width: buttonWidth,
+        borderRadius: "0", // No rounded corners
+      };
+
+      return (
+        <button
+          key={`${date}-${hour}`}
+          style={buttonStyles}
+          onClick={() => handleButtonClick(date, hour)}
+        >
+          {hour}:00
+        </button>
+      );
+    });
+
+    // Wrapping buttons in a div for each hour
+    return (
+      <div key={`row-${hour}`} className="hourly-row">
+        {buttons}
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>TimeHour Page</h1>
@@ -76,20 +90,32 @@ export default function TimeHour() {
           ))}
         </ul>
       </div>
-      <div className="hourly-grid">
-        {hourlyGrid.map((row, index) => (
-          <div key={index} className="hourly-row">
-            {row}
+          <div className="hourly-grid flex">
+            <div className="hour-label-column">
+          {hours.map((hour) => (
+          <div
+            key={`label-${hour}`}
+            className="hour-label h-9" 
+          >
+            {hour > 12 ? `${hour - 12}pm` : `${hour}am`}
+          </div>
+          ))}
+      </div>
+          <div className="hourly-grid-item" style={{ display: 'block' }}>
+        {hourlyGrid.map((item, index) => (
+          <div key={`grid-item-${index}`}>
+            {item}
           </div>
         ))}
       </div>
+          </div>
       <button onClick={groupButtonsByRow}>Save</button>
       <div>
         <h2>Saved Selection:</h2>
         <ul>
           {formattedDates.map((formattedDate, index) => (
             <li key={index}>{formattedDate}</li>
-           ) )}
+          ))}
         </ul>
       </div>
     </div>
