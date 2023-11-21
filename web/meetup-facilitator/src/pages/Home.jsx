36908d837e213de642013d2b3c28fe76b4989483
Chris Chan <christakleungchan@gmail.com>
2023-11-21 11:28:15 -0500

    Add missing </Link> ending tag

--- web/meetup-facilitator/src/pages/Home.jsx
+++ web/meetup-facilitator/src/pages/Home.jsx
@@ -70,7 +70,6 @@ function Home() {
         })
         .then((response) => console.log(response.json()))
 
-        
 
     }
 
@@ -78,19 +77,20 @@ return (
     <>
         <Navbar />
         <div className='flex-col flex my-3 gap-1'>
-        <label className='text-2xl' htmlFor='new-group-name'>Group Name:</label>
-        <input ref={groupName} onChange={onChange} className={`border-2 p-2 ${(isValid ? '' : 'border-red-500')} focus:outline-none`} name='new-group-name' id='new-group-name' type='text' placeholder='Enter the group name here.'/>
-        <div className={`text-red-500${(isValid) ? ' invisible' : ' visible'}`}>Please enter a valid group name.</div>
-      </div>
-      <Button click={onSubmit} classList={buttonStyles} text='Create Group' />  
+            <label className='text-2xl' htmlFor='new-group-name'>Group Name:</label>
+            <input ref={groupName} onChange={onChange} className={`border-2 p-2 ${(isValid ? '' : 'border-red-500')} focus:outline-none`} name='new-group-name' id='new-group-name' type='text' placeholder='Enter the group name here.'/>
+            <div className={`text-red-500${(isValid) ? ' invisible' : ' visible'}`}>Please enter a valid group name.</div>
+        </div>
+        <Button click={onSubmit} classList={buttonStyles} text='Create Group' />  
 
-      <Link to="/location">
-        <Button classList={buttonStyles} text="Preferences" />
+        <Link to="/location">
+            <Button classList={buttonStyles} text="Preferences" />
+        </Link>
 
-      <Link to="/time">
-        <button className="fixed bottom-4 right-4 bg-green-700 text-white p-2 rounded">
-          Next: Time Selection
-        </button>
+        <Link to="/time">
+            <button className="fixed bottom-4 right-4 bg-green-700 text-white p-2 rounded">
+            Next: Time Selection
+            </button>
 
       </Link>
     </>
