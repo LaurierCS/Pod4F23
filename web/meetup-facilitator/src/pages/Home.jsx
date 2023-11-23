42e449e30e38310490db3ebd96f51c91419d2a4b
Chris Chan <christakleungchan@gmail.com>
2023-11-23 14:29:45 -0500

    Fix syntax errors

--- web/meetup-facilitator/src/pages/Home.jsx
+++ web/meetup-facilitator/src/pages/Home.jsx
@@ -78,14 +78,15 @@ return (
     <>
         <Navbar />
         <div className='flex-col flex my-3 gap-1'>
-        <label className='text-2xl' htmlFor='new-group-name'>Group Name:</label>
-        <input ref={groupName} onChange={onChange} className={`border-2 p-2 ${(isValid ? '' : 'border-red-500')} focus:outline-none`} name='new-group-name' id='new-group-name' type='text' placeholder='Enter the group name here.'/>
-        <div className={`text-red-500${(isValid) ? ' invisible' : ' visible'}`}>Please enter a valid group name.</div>
-      </div>
+            <label className='text-2xl' htmlFor='new-group-name'>Group Name:</label>
+            <input ref={groupName} onChange={onChange} className={`border-2 p-2 ${(isValid ? '' : 'border-red-500')} focus:outline-none`} name='new-group-name' id='new-group-name' type='text' placeholder='Enter the group name here.'/>
+            <div className={`text-red-500${(isValid) ? ' invisible' : ' visible'}`}>Please enter a valid group name.</div>
+        </div>
       <Button click={onSubmit} classList={buttonStyles} text='Create Group' />  
 
       <Link to="/location">
         <Button classList={buttonStyles} text="Preferences" />
+      </Link>
 
       <Link to="/time">
         <button className="fixed bottom-4 right-4 bg-green-700 text-white p-2 rounded">
